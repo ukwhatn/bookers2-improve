273d4025672ff1a16f1be763be86a6cbf282f506
Yuki Watanabe <ukwhatn@gmail.com>
2023-03-03 14:36:35 +0900

    add: books#indexでのソート機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,13 +12,18 @@ class BooksController < ApplicationController
 
   def index
     #@books = Book.all
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.all.sort {
-      |a, b|
-      b.favorites.where(created_at: from...to).size <=>
-        a.favorites.where(created_at: from...to).size
-    }
+    case params[:sort]
+    when "favorites" then
+      to = Time.current.at_end_of_day
+      from = (to - 6.day).at_beginning_of_day
+      @books = Book.all.sort {
+        |a, b|
+        b.favorites.where(created_at: from...to).size <=>
+          a.favorites.where(created_at: from...to).size
+      }
+    else
+      @books = Book.all.order(params[:sort])
+    end
 
     @book = Book.new
   end
